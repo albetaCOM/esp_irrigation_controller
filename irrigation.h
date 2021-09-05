112d34f75906eaf4b2227084f62cad6fd30544cc
Alba Prades Doria <alba.prades@idneo.com>
2021-09-05 20:20:41 +0200

    Add protection if the string time is not well formatted

--- irrigation.h
+++ irrigation.h
@@ -1,6 +1,3 @@
-// Forked from: https://github.com/brianhanifin/esphome-config/blob/master/irrigation.h
-
-
 #include "esphome.h"
 using namespace std;
 
@@ -60,21 +57,25 @@ string update_next_runtime(string time_list) {
   // Compare the list of times with the current time, and return the next in the list.
   //ESP_LOGD("update_next_runtime", "now: %i:%i", hour, minute);
   for (string time : times) {
-    // Retrieve the next scheduled time from the list.
-    next_hour = atoi(time.substr(0,2).c_str());
-    next_minute = atoi(time.substr(3,2).c_str());
-
-    //ESP_LOGD("update_next_runtime", "next_hour: %s", time.c_str());
-    if (time_hour < next_hour || (time_hour == next_hour && time_minute < next_minute)) {
-      // Return this time if the next hour is greater than the current hour.
-      return times[loop_count].c_str();
-      break;
-    // When we reach the end of our schedule for the day, return the first time of tomorrow.
-    } else if (time_count == loop_count) {
-      return times[0].c_str();
-      break;
+    if (time.length() >= 5) {
+        // Retrieve the next scheduled time from the list.
+        next_hour = atoi(time.substr(0,2).c_str());
+        next_minute = atoi(time.substr(3,2).c_str());
+    
+        //ESP_LOGD("update_next_runtime", "next_hour: %s", time.c_str());
+        if (time_hour < next_hour || (time_hour == next_hour && time_minute < next_minute)) {
+          // Return this time if the next hour is greater than the current hour.
+          return times[loop_count].c_str();
+          break;
+        // When we reach the end of our schedule for the day, return the first time of tomorrow.
+        } else if (time_count == loop_count) {
+          return times[0].c_str();
+          break;
+        }
+    } else {
+        ESP_LOGW("update_next_runtime()", "loop_count: %i, time: '%s'", loop_count, time.c_str());
+        return "unknown";
     }
-
     // Increment the loop counter and array index.
     loop_count += 1;
     index += 2;
